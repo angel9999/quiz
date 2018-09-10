fcf87f19a8a2ea226fd456a51f0c5c846cd8041a
angel9999 <31519796+angel9999@users.noreply.github.com>
2018-09-10 10:16:16 -0700

    Add files via upload

---
+++ mysqript.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	$("#p").click(function() {
+		$(".1").toggle();
+	})
+})
+$(document).ready(function(){
+	$("img").click(function() {
+		$("img").hide();
+	})
+})
+$(document).ready(function(){
+	$("h1").click(function() {
+		$("h1").hide();
+	})
+})
+$(document).ready(function(){
+	$("pre").click(function() {
+		$("pre").hide();
+	})
+})
