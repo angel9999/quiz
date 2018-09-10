dd76c3ea7306bfd74bf14b25fef16fdee2e77990
angel9999 <31519796+angel9999@users.noreply.github.com>
2018-09-10 10:39:41 -0700

    Update mysqript.js

--- mysqript.js
+++ mysqript.js
@@ -1,20 +1,17 @@
 $(document).ready(function(){
 	$("#p").click(function() {
-		$(".1").toggle();
-	})
-})
-$(document).ready(function(){
-	$("img").click(function() {
-		$("img").hide();
-	})
-})
-$(document).ready(function(){
+	$("#p2").toggle();})
+	$("#p3").click(function() {
+	$("#p4").toggle();})
+	$("#p5").click(function() {
+	$("#p6").toggle();})
+	$("#p7").click(function() {
+	$("#p8").toggle();})
 	$("h1").click(function() {
-		$("h1").hide();
-	})
-})
-$(document).ready(function(){
-	$("pre").click(function() {
-		$("pre").hide();
+		$(".disappear").toggle();
+	$("p").css({"color":"red","font-size":"30px"});
+	$("h1").css({"font-size":"50px"});
+	$("p").css({"text-align":"center"});
+	$("h1").css({"text-align":"center"});
 	})
 })
